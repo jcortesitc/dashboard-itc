e5091e9073cbdf8c41403b96f8b2bea04cbd08e6
Jorge Cortes <jcortes@itconsultants.cl>
2024-05-17 15:51:04 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+
+      <iframe title="jira_ing" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYmE3NTA3NWQtNzI5NS00MTIyLTgzNTMtM2QwNzY0YWU4NTMwIiwidCI6IjMxZDdkOTk5LTJmZjItNGJmMi04MTIxLWYwZGM0YzUyYmQ0MiIsImMiOjR9&pageName=ReportSectione5486eaaf2949c164817" frameborder="0" allowFullScreen="true"></iframe>    </>
+  )
+}
+
+export default App
