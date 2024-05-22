c5f0249e9963f6d34f0b0bcc9c19d9f55cb61914
Jorge Cortes <jcortes@itconsultants.cl>
2024-05-22 13:50:15 -0400

    navbar multi reporte

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Report from './components/Report/Report.jsx'
 
 function App() {
 
 
   return (
     <>
-      <iframe title="jira_ing" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYmE3NTA3NWQtNzI5NS00MTIyLTgzNTMtM2QwNzY0YWU4NTMwIiwidCI6IjMxZDdkOTk5LTJmZjItNGJmMi04MTIxLWYwZGM0YzUyYmQ0MiIsImMiOjR9&pageName=ReportSectione5486eaaf2949c164817" frameBorder="0" allowFullScreen={true}
-      ></iframe>
+      <Navbar className="navbar fixed-top position-absolute top-0"/>
+      <Report />
     </>
   )
 }
---
+++ src/components/Report/Report.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+
+let pbi_report1 = 'https://app.powerbi.com/view?r=eyJrIjoiNDA0YmIzOGYtYWRkYS00ZmJjLThmNGItMmJjMThiMmVhNDcxIiwidCI6IjMxZDdkOTk5LTJmZjItNGJmMi04MTIxLWYwZGM0YzUyYmQ0MiIsImMiOjR9&pageName=ReportSection'
+let pbi_report2 = 'https://app.powerbi.com/view?r=eyJrIjoiYmE3NTA3NWQtNzI5NS00MTIyLTgzNTMtM2QwNzY0YWU4NTMwIiwidCI6IjMxZDdkOTk5LTJmZjItNGJmMi04MTIxLWYwZGM0YzUyYmQ0MiIsImMiOjR9&pageName=ReportSectione5486eaaf2949c164817'
+
+const Report = () => {
+    const [report, setReport] = useState([''])
+
+
+
+
+
+
+    return (
+        <div>
+            <h1 className="title text-center mb-2">Reportes</h1>
+            <p>Seleccione un reporte para desplegar</p>
+            <button className="btn btn-primary mx-1" onClick={() => setReport(pbi_report1)}>Reporte 1</button>
+            <button className="btn btn-primary mx-1" onClick={() => setReport(pbi_report2)}>Reporte 2</button>
+            <div>
+            <iframe title="powerbi" width="600" height="400" src={report} allowFullScreen={true}></iframe>
+            </div>
+        </div>
+    )
+}
+
+export default Report
